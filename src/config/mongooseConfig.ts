50dc493f7c5c9965d95245fa8cd22c67c07a04b7
Alok515 <70854387+Alok515@users.noreply.github.com>
2023-12-07 17:32:55 +0530

    Update mongooseConfig.ts

--- src/config/mongooseConfig.ts
+++ src/config/mongooseConfig.ts
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 
 const MongooseConnect = async ()=> {
-    const connection = await mongoose.connect("mongodb+srv://alok28:alok108@alok.lpdwxfo.mongodb.net/gmail?&retryWrites=true&w=majority");
+    const connection = await mongoose.connect("");
     if(connection){
         console.log('Connected to Mongoose server');
     }
