bbb553be5ec23a9da3b7a1a7109f20c508672f8b
Alok515 <70854387+Alok515@users.noreply.github.com>
2023-12-07 17:32:29 +0530

    Update mongooseConfig.js

--- dist/config/mongooseConfig.js
+++ dist/config/mongooseConfig.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 const MongooseConnect = async () => {
-    const connection = await mongoose_1.default.connect("mongodb+srv://alok28:alok108@alok.lpdwxfo.mongodb.net/gmail?&retryWrites=true&w=majority");
+    const connection = await mongoose_1.default.connect("");
     if (connection) {
         console.log('Connected to Mongoose server');
     }
